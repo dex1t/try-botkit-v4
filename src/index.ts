48a8c28f3150cba0982583f24ad7860cae95f85b
dex1t <dex1t@deg.ooo>
2020-01-16 08:33:02 +0900

    Implement slash command, and webhook endpoint for itnercom

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Botkit } from "botkit";
-import { SlackAdapter, SlackEventMiddleware } from "botbuilder-adapter-slack";
+import {
+  SlackAdapter,
+  SlackEventMiddleware,
+  SlackMessageTypeMiddleware
+} from "botbuilder-adapter-slack";
 
 const adapter = new SlackAdapter({
   clientSigningSecret: process.env.SLACK_CLIENT_SIGNING_SECRET,
@@ -7,12 +11,15 @@ const adapter = new SlackAdapter({
   redirectUri: ""
 });
 adapter.use(new SlackEventMiddleware());
+adapter.use(new SlackMessageTypeMiddleware());
 
 const controller = new Botkit({
   adapter: adapter
 });
 
-controller.webserver.get("/", (req, res) => {
+let memory = null; // TODO: make persistent to Redis or Mongo
+
+controller.webserver.get("/", async (req, res) => {
   res.send(`It works! This app is running Botkit ${controller.version}.`);
 });
 
@@ -22,4 +29,28 @@ controller.on("app_mention", async (bot, message) => {
   } else {
     await bot.reply(message, "I received mention :+1:");
   }
+});
+
+controller.on("slash_command", async (bot, message) => {
+  console.log(message);
+
+  if (message.command == "/whisperbot-subscribe") {
+    console.log(message.reference);
+    memory = message.reference;
+    // @ts-ignore there is implementation, but no types
+    await bot.replyPrivate(
+      message,
+      `OK :+1: I'm subscribing \`${message.text}\` on Intercom.`
+    );
+  }
+});
+
+controller.webserver.get("/intercom", async (req, res) => {
+  if (memory) {
+    const bot2 = await controller.spawn(memory.conversation.id);
+    // @ts-ignore there is implementation, but no types
+    await bot2.startConversationInChannel(memory.conversation.id, memory.user);
+    await bot2.say("got webhook");
+  }
+  res.send({});
 });
